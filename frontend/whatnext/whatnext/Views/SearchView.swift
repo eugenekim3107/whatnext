3d9bfb17530b893f52af165320cf5977a90a689c
eugenekim <eugenekim3107@gmail.com>
2024-01-30 13:15:16 -0800

    edited files

--- frontend/whatnext/whatnext/Views/SearchView.swift
+++ frontend/whatnext/whatnext/Views/SearchView.swift
@@ -7,9 +7,37 @@
 
 import SwiftUI
 
+//Created by Wenzhou Lyu
 struct SearchView: View {
+    @State private var searchText = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            NavigationView {
+                VStack {
+                    Spacer()
+                    Image("logo-icon") // Replace with your logo image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                    Text("Let's find your next favorite spot!")
+                        .font(.headline)
+                    Text("What are you in the mood for?")
+                        .font(.headline)
+                    Image("openai-icon")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120, height: 120,alignment:.top)
+                    Spacer(minLength:270)
+                    TextField("Type your message here...", text: $searchText)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal, 50)
+                    Spacer()
+                }
+            }
+
+            // Add other tabs here...
+        }
     }
 }
 
